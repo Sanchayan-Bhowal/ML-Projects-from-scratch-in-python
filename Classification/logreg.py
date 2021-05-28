73eae19122acfe6ad199ddc29897d1a698658934
Sanchayan-Bhowal <sanchayan.bhowal2509@gmail.com>
2021-05-28 15:10:51 +0530

    logistic regression simple bugs fixed

--- Classification/logreg.py
+++ Classification/logreg.py
@@ -1,9 +1,11 @@
 from numpy import *
 from matplotlib.pyplot import *
 
+def h_sub(o):
+    return array(list(map(lambda x:1/(1+math.exp(-x)),o)))
 def h(o):
-    z=dot(X,o)
-    return array(list(map(lambda x:1/(1+math.exp(-x)),z)))
+    t=dot(X,o)
+    return array(list(map(lambda x:1/(1+math.exp(-x)),t)))
 
 def cost(theta):
     return 0.5*sum((h(theta)-y)**2)
@@ -24,11 +26,13 @@ theta=zeros(x.shape[1]+1)
 X=concatenate([ones((m,1)),x],axis=1)
 theta,storecost=gradientdescent(theta)
 
-# pltx,plty=meshgrid(linspace(-2,3.2,10),linspace(-3,3,10))
-# z=theta[0]+theta[1]*pltx+theta[2]*plty
-# plt3d=figure()
-# ax=plt3d.add_subplot(111,projection='3d')
-# ax.plot_surface(pltx,plty,z,alpha=0.2)
-# ax.scatter3D(x[:,0],x[:,1],y)
-plot(arange(1,1001),storecost)
+pltx,plty=meshgrid(linspace(-2,3.2,10),linspace(-3,3,10))
+temp=theta[0]+theta[1]*pltx+theta[2]*plty
+z=apply_along_axis(lambda x:h_sub(x),axis=0,arr=temp)
+
+plt3d=figure()
+ax=plt3d.add_subplot(111,projection='3d')
+ax.plot_surface(pltx,plty,z,alpha=0.2)
+ax.scatter3D(x[:,0],x[:,1],y)
+# plot(arange(1,1001),storecost)
 show()
